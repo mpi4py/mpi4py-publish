c5850c64003672501d45e49706397b6a5eb71dc7
Lisandro Dalcin <dalcinl@gmail.com>
2024-03-19 11:49:02 +0300

    auditwheel: Support release 6+

--- .cibw/auditwheel.py
+++ .cibw/auditwheel.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 import sys
 
+from auditwheel import policy
 from auditwheel.main import main
-from auditwheel.policy import load_policies
 
 libmpi = []
 for name in ("mpi", "open-pal", "open-rte"):
     for version in (1, 12, 20, 40):
         libmpi.append(f"lib{name}.so.{version}")
 
-for policy in load_policies():
-    policy["lib_whitelist"].extend(libmpi)
+if hasattr(policy, "WheelPolicies"):
+
+    super_init = policy.WheelPolicies.__init__
+
+    def init(self, *args, **kwargs):
+        super_init(self, *args, **kwargs)
+        for entry in self.policies:
+            entry["lib_whitelist"].extend(libmpi)
+
+    policy.WheelPolicies.__init__ = init
+
+else:
+
+    for entry in policy.load_policies():
+        entry["lib_whitelist"].extend(libmpi)
 
 if __name__ == "__main__":
     sys.exit(main())
