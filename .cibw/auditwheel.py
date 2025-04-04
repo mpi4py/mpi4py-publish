318d804712f7692e8a45dbc3dd68e552cf7343fb
Lisandro Dalcin <dalcinl@gmail.com>
2025-04-04 20:18:08 +0300

    auditwheel: Update after release 6.3+

--- .cibw/auditwheel.py
+++ .cibw/auditwheel.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import sys
+
 from auditwheel.main import main
 
-for name in ("mpi", "open-pal", "open-rte"):
-    sys.argv.append("--exclude")
-    sys.argv.append(f"lib{name}.so.*")
+if "repair" in sys.argv:
+    sys.argv.append("--only-plat")
+    for name in ("mpi", ):
+        sys.argv.append("--exclude")
+        sys.argv.append(f"lib{name}.so.*")
 
-if __name__ == "__main__":
-    sys.exit(main())
+sys.exit(main())
